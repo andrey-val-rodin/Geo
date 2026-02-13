22d2f2f33fb52b7b0a54b8032089f312b014bc68
Andrey Rodin <andrey.val.rodin@gmail.com>
2026-02-13 20:32:54 +0300

    Further development

--- Geo/MainPage.xaml.cs
+++ Geo/MainPage.xaml.cs
@@ -1,5 +1,8 @@
-﻿using Mapsui.Tiling;
-using Mapsui.UI.Maui;
+﻿using Geo.Services;
+using Mapsui;
+using Mapsui.Projections;
+using Mapsui.Tiling;
+using System.Globalization;
 
 namespace Geo
 {
@@ -9,9 +12,75 @@ namespace Geo
         {
             InitializeComponent();
 
-            var mapControl = new MapControl();
-            mapControl.Map?.Layers.Add(OpenStreetMap.CreateTileLayer());
-            Content = mapControl;
+            MapElement.Map?.Layers.Add(OpenStreetMap.CreateTileLayer());
+            MapElement.Map?.Widgets.Clear();
+        }
+
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+            await InitializeAsync();
+        }
+
+        private async Task InitializeAsync()
+        {
+            ShowProgress();
+
+            var location = await Locator.GetCurrentLocationAsync(CancellationToken.None);
+            if (location != null)
+            {
+                var latitude = location.Latitude.ToString(CultureInfo.InvariantCulture);
+                var longitude = location.Longitude.ToString(CultureInfo.InvariantCulture);
+                TitleLabel.Text = $"{latitude}, {longitude}";
+
+                DisplayLocation(location);
+            }
+
+            HideProgress();
+        }
+
+        private void ShowProgress()
+        {
+            LoadingIndicator.IsRunning = true;
+            LoadingIndicator.IsVisible = true;
+        }
+
+        private void HideProgress()
+        {
+            LoadingIndicator.IsRunning = false;
+            LoadingIndicator.IsVisible = false;
+        }
+
+        private bool DisplayLocation(Location location)
+        {
+            ArgumentNullException.ThrowIfNull(location, nameof(location));
+
+            try
+            {
+                // Transform coordinates
+                var point = SphericalMercator.FromLonLat(new MPoint(location.Longitude, location.Latitude));
+
+                // Move map to coordinates
+                MapElement.Map?.Navigator?.CenterOnAndZoomTo(point, 4, 1000);
+
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        private async void OnTitleTapped(object sender, EventArgs e)
+        {
+            if (sender is not Label label)
+                return;
+
+            string coordinates = label.Text;
+            await Clipboard.Default.SetTextAsync(coordinates);
+            label.Text = "Скопировано в буфер обмена!";
+            await Task.Delay(1000);
+            label.Text = coordinates;
         }
     }
 }
---
+++ Geo/Services/Locator.cs
@@ -0,0 +1,40 @@
+﻿namespace Geo.Services
+{
+    public static class Locator
+    {
+        public static async Task<Location> GetCurrentLocationAsync(CancellationToken token)
+        {
+            try
+            {
+                var request = new GeolocationRequest(GeolocationAccuracy.Medium, TimeSpan.FromSeconds(10));
+
+                Location location = await Geolocation.Default.GetLocationAsync(request, token);
+
+                if (location != null)
+                {
+                    // TODO
+                    double lat = location.Latitude;
+                    double lng = location.Longitude;
+                    double? altitude = location.Altitude; // может быть null
+                                                          // используйте координаты
+                    return location;
+                }
+            }
+            catch (FeatureNotSupportedException ex)
+            {
+                // Геолокация не поддерживается на устройстве или
+                // службы геолокации (GPS) выключены в настройках устройства.
+            }
+            catch (PermissionException ex)
+            {
+                // Пользователь не выдал разрешение
+            }
+            catch (Exception ex)
+            {
+                // Прочие ошибки (например, выключен GPS)
+            }
+
+            return null;
+        }
+    }
+}
